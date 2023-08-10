41fab2c3cd54083db83f98ea88f45405d52158b8
jaworskiwojciech <jaworski.wojciech94@gmail.com>
2023-08-10 19:05:02 +0200

    skonstruowano api

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,60 @@
 const express = require("express");
+const {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+  updateContact,
+} = require("../../models/contacts");
+
+const { schemaAdd, schemaUpdate } = require("../../validation/validation");
 
 const router = express.Router();
 
 router.get("/", async (req, res, next) => {
-  res.json({ message: "henlo world" });
+  const contacts = listContacts();
+  res.status(200).json({ message: contacts });
 });
 
 router.get("/:contactId", async (req, res, next) => {
-  res.json({ message: "template message" });
+  const { contactId } = req.params;
+  const response = await getContactById(contactId);
+  response
+    ? res.status(200).json({ message: response })
+    : res.status(404).json({ message: "Not found" });
 });
 
 router.post("/", async (req, res, next) => {
-  res.json({ message: "template message" });
+  try {
+    const body = await schemaAdd.validateAsync(req.body);
+    const response = await addContact(body);
+    res.status(200).json({ message: response });
+  } catch (error) {
+    res.status(400).json({ message: "missing required name - field" });
+    console.log(error);
+  }
 });
 
 router.delete("/:contactId", async (req, res, next) => {
-  res.json({ message: "template message" });
+  const { contactId } = req.params;
+  const response = await removeContact(contactId);
+  response
+    ? res.status(200).json({ message: "Contact deleted!" })
+    : res.status(404).json({ message: "Not found!" });
 });
 
 router.put("/:contactId", async (req, res, next) => {
-  res.json({ message: "template message" });
+  try {
+    const { contactId } = req.params;
+    const updatedContact = await schemaUpdate.validateAsync(req.body);
+    const response = await updateContact(contactId, updatedContact);
+    response
+      ? res.status(200).json({ message: response })
+      : res.status(404).json({ message: "Not found" });
+  } catch (error) {
+    res.status(400).json({ message: "Missing fields" });
+    console.log(error);
+  }
 });
 
 module.exports = router;
