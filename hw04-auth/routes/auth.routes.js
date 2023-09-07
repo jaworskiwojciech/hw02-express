ff66f29ab999bd009882736130b11fd157b12e72
jaworskiwojciech <jaworski.wojciech94@gmail.com>
2023-09-07 13:12:56 +0200

    bugfix

--- hw04-auth/routes/auth.routes.js
+++ hw04-auth/routes/auth.routes.js
@@ -5,7 +5,7 @@ const auth = require("../middlewares/auth");
 
 router.post("/login", authController.login);
 router.post("/signup", authController.signup);
-router.get("/logout/:id", auth, authController.logout);
+router.get("/logout", auth, authController.logout);
 router.get("/current", auth, authController.current);
 
 module.exports = router;
